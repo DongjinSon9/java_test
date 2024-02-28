99e06bcd7cd8313e2749b5384610f79ddb1183f9
DongjinSon9 <dongj1358@naver.com>
2024-02-28 17:37:16 +0900

    8교시 - 연산자, if문

---
+++ JAVAProjectEX240228/src/ex_240228/Ex_4_Operator.java
@@ -0,0 +1,46 @@
+package ex_240228;
+
+import java.net.MulticastSocket;
+import java.security.PublicKey;
+import java.util.Scanner;
+
+public class Ex_4_Operator {
+
+	public static void main(String[] args) {
+
+		System.out.println("전위 및 후위 연산자 확인해보기");
+		int a = 1;
+		int b = ++a;
+		System.out.println("a :" + a + ", b : " + b);
+	// a의 값을 증가시키고 b에 할당.
+		
+		int a2 = 1;
+		int b2 = a2++;
+		System.out.println("a2 :" + a2 + ", b2 : " + b2);
+	// a2의 값을 b2에 할당한 뒤에 a2의 값을 증가.
+	// 후위 연산자는 반복문에서 주로 사용한다.
+		char chartest1 = 'a';
+		char chartest2 = 'b';
+		int chartest1toint = chartest1;
+		int chartest2toint = chartest2;
+		System.out.println("char \'a\'에 해당하는 유니코드 값 십진수 표현 : " + chartest1toint);
+		System.out.println("char \'b\'에 해당하는 유니코드 값 십진수 표현 : " + chartest2toint);
+		
+	// 삼항다항식   (조건식)?true:false
+		int x =10;
+		int y= 12;
+		int result = (x>y)?x:y;
+		System.out.println(result);
+		
+		int big;
+		if(x>y) {
+			big = x;
+		}
+		else {
+				big = y;
+			}
+	
+
+	}
+
+}
---
+++ JAVAProjectEX240228/src/ex_240228/Ex_5_Operator2.java
@@ -0,0 +1,21 @@
+package ex_240228;
+
+import java.util.Scanner;
+
+public class Ex_5_Operator2 {
+
+	public static void main(String[] args) {
+
+	// 클래스명 참조형변수 = new 클래스명(인자값);
+	// system.in, 표준입력, 키보드로 입력된 값을 바이트로 변환
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("정수를 입력해주세요 >> ");
+	// 콘솔에 입력된 정수를 담을 변수를 지정
+		int time = scanner.nextInt();		
+		int minute = time/60;
+		System.out.println("입력하신 정수의 분은 " + minute + "분 입니다.");
+
+
+	}
+
+}
