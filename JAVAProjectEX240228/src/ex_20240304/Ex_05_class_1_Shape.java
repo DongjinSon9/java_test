273003b6b4b682ddf6d9bd4f066ff65482a8c7c8
DongjinSon9 <dongj1358@naver.com>
2024-03-04 15:57:35 +0900

    7교시 - 접근지정자 확인

--- JAVAProjectEX240228/src/ex_20240304/Ex_05_class_1_Shape.java
+++ JAVAProjectEX240228/src/ex_20240304/Ex_05_class_1_Shape.java
@@ -3,8 +3,13 @@ package ex_20240304;
 public class Ex_05_class_1_Shape {
 
 	public static void main(String[] args) {
+		// 자식 클래스에서 부모 클래스의 접근 지정자가 protected이면 보인다
 		Triangle tri1 = new Triangle();
-		String protectedString
+		int protectedVarString = tri1.edgePoint;
+		
+		// 자식 클래스가 아니라서 상속을 안 받았고 protected가 안 보인다
+		Rectangle rec1 = new Rectangle();
+		rec1.
 	}
 
 }
--- JAVAProjectEX240228/src/ex_20240304/Rectangle.java
+++ JAVAProjectEX240228/src/ex_20240304/Rectangle.java
@@ -1,6 +1,10 @@
 package ex_20240304;
 
-public class Rectangle { // shape 상속을 안 받고 일반 멤버 추가
+public class Rectangle { 
+	// shape 상속을 안 받고 일반 멤버 추가
+	// protected에 접근이 되는지 확인
+	
+	private String edgeColor;
 	
 
 }
