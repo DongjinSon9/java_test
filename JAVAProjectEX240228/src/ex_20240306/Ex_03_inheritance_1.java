c03d1714317d7dc8db57c9114c198bf0d2f261ff
DongjinSon9 <dongj1358@naver.com>
2024-03-06 12:30:23 +0900

    4교시 - instanceof 다형성 레퍼런스 확인

--- JAVAProjectEX240228/src/ex_20240306/Ex_03_inheritance_1.java
+++ JAVAProjectEX240228/src/ex_20240306/Ex_03_inheritance_1.java
@@ -2,6 +2,8 @@ package ex_20240306;
 
 // 부모 클래스
 // 클래스 정의
+
+// 클래스 구조 : C -> B -> A2
 class A2 {
 	public A2() {
 		System.out.println("생성자a2");
@@ -29,7 +31,24 @@ public class Ex_03_inheritance_1 {
 		System.out.println("부모 클래스 먼저 생성자 호출 확인.");
 		// 디버깅 해보기 브레이크 포인트 잡고
 		// 디버그 실행 ( f11)
-
+		
+		
+		// instance of 예제
+		// 다형성. 업캐스팅
+		// 문제점 : 원래의 객체를 알기가 어렵다.
+		A2 test1 = new A2();
+		A2 test2 = new B();
+		A2 test3 = new C();
+		
+		boolean result1 = (test2 instanceof B) ? true : false;
+		System.out.println("test2 instanceof B : " + result1);
+		
+		boolean result2 = (test2 instanceof A2) ? true : false;
+		System.out.println("test2 instanceof A2 : " + result2);
+		
+		boolean result3 = (test2 instanceof C) ? true : false;
+		System.out.println("test2 instanceof C : " + result3);
+		
 	}
 
 }
